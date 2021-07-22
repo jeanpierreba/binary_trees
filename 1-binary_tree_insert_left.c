e32d42d0ed8caa49594dff9f0da4abbe90dd16c9
jeanpierreba <2713@holbertonschool.com>
2021-07-22 11:55:01 -0500

    changed malloc to function call

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,20 +13,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
-	new_node->left = parent->left;
-	parent->left = new_node;
 	if (parent->left)
 	{
+		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
+	parent->left = new_node;
 
 	return (new_node);
 }
